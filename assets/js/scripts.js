6b9dd33dc415dfa7539f5eff4dad497e32f4dd81
bowenxu <xubowenhaoren@gmail.com>
2022-11-13 20:54:21 -0800

    Finished editing the gala page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,20 @@ menuTrigger.onclick = function() {
   menuTrigger.classList.toggle('is-active');
   body.classList.toggle('lock-scroll');
 };
+
+function openTab(evt, tabName) {
+  let i;
+  const tabContent = document.getElementsByClassName('tab-content');
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
+  }
+  const tabLinks = document.getElementsByClassName('tab-links');
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(' active', '');
+  }
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById('defaultOpen').click();
