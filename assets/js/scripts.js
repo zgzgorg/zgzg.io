9bcb01e8d888ed85964e570eb89af121d494dd2f
Leo Mei <liqiangmei@outlook.com>
2023-12-23 00:26:15 -0800

    fixed tabs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,19 +11,19 @@ menuTrigger.onclick = function() {
   body.classList.toggle('lock-scroll');
 };
 
-// function openTab(evt) {
-//   const tabContents = document.getElementsByClassName('tab-content');
-//   for (const tabContent of tabContents) {
-//     tabContent.style.display = 'none';
-//   }
-//   const tabLinks = document.getElementsByClassName('tab-links');
-//   for (const tabLink of tabLinks) {
-//     tabLink.classList.remove('active');
-//   }
-//   const tabName = evt.currentTarget.dataset.tabId;
-//   document.getElementById(tabName).style.display = 'block';
-//   evt.currentTarget.className += ' active';
-// }
+function openTab(evt) {
+  const tabContents = document.getElementsByClassName('tab-content');
+  for (const tabContent of tabContents) {
+    tabContent.style.display = 'none';
+  }
+  const tabLinks = document.getElementsByClassName('tab-links');
+  for (const tabLink of tabLinks) {
+    tabLink.classList.remove('active');
+  }
+  const tabName = evt.currentTarget.dataset.tabId;
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
 
 // Get the element with class="default-open" and click on it
 const defaultOpenTabs = document.getElementsByClassName('default-open');
